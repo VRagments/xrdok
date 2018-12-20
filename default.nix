12e0bfd246b1ec18ccfcfbac6e33f3cac889cbf2
Ronny Esterluss <ronny@vragments.com>
2018-12-20 11:06:47 +0100

    updated default nix to use latest firefox

--- default.nix
+++ default.nix
@@ -1,4 +1,9 @@
 let
+  unstable = import (fetchTarball { # nixos-unstable
+    url = https://github.com/NixOS/nixpkgs-channels/archive/c26dbef830a.tar.gz;
+    sha256 = "1bh3c7in7lfainbak1dmn9kwaba07z6wc65g99ylyahcy5g4pb0w";
+  }) {};
+
   pkgs = import (fetchTarball { # 18.09
     url = https://github.com/NixOS/nixpkgs-channels/archive/7795a7ad5f0.tar.gz;
     sha256 = "1kh35ys6f60x35bsav64alqwnzps3xc21m1ayrcms005ylryi34c";
@@ -7,8 +12,7 @@ in with pkgs; {
   pogstateEnv = stdenv.mkDerivation {
     name = "xrdok";
     buildInputs = [
-      arcanist
-      firefox-devedition-bin
+      unstable.firefox-devedition-bin
       inotifyTools
       nodejs-8_x
       stdenv
